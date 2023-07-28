f1a9d354395f7d3e1245eeaa7080afe65adb88ea
Timothy Ho <timothyhohyhtomit@gmail.com>
2023-07-27 20:06:58 -0700

    worked on CommandWindow

--- client/src/containers/main/Main.js
+++ client/src/containers/main/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Header from "../../components/header/Header";
 import FileExplorer from "../../components/file-explorer/FileExplorer";
@@ -7,15 +7,41 @@ import VariableStorage from "../../components/variable-storage/VariableStorage";
 import CommandWindow from "../../components/command-window/CommandWindow";
 
 import "./Main.css";
+import { compute } from "../../util/eval";
 
 function Main() {
+    /* states */
+    // CommandWindow
+    const [queryHistory, setQueryHistory] = useState([]);  // history of submitted queries
+    const [query, setQuery] = useState("");  // query shown in input element
+    /* handlers */
+    // CommandWindow
+    const handleKeyUpQuery = (e) => {
+        if (e.keyCode !== 13) return;
+        // if enter was pressed, compute result
+        const result = compute(query);
+        // add query to history
+        const newItem = {
+            query,
+            result
+        };
+        setQueryHistory((prev) => {
+            return [...prev, newItem];
+        });
+    }
     return (
         <div className="main">
             <Header />
             <FileExplorer />
             <Editor />
             <VariableStorage />
-            <CommandWindow />
+            <CommandWindow
+                queryHistory={queryHistory}
+                setQueryHistory={setQueryHistory}
+                query={query}
+                setQuery={setQuery}
+                handleKeyUpQuery={handleKeyUpQuery}
+            />
         </div>
     );
 }
---
+++ client/src/util/eval.js
@@ -0,0 +1,7 @@
+/*
+eval.js provides helper functions for computing numerical result from a query in string format.
+*/
+
+export const compute = (query) => {
+    return query.length;
+};
